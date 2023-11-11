724a4010d68e4dfc6bd800722de6e4b4bf6d3035
eiribja <eiribja@uio.no>
2023-11-11 16:43:54 +0100

    silly non working code

--- project2/src/logisticRegression.py
+++ project2/src/logisticRegression.py
@@ -1,7 +1,7 @@
 import ADAMLL as ada
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import  train_test_split 
-import numpy as np
+import jax.numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -9,7 +9,12 @@ cancer = load_breast_cancer()
 
 X_train, X_test, y_train, y_test = train_test_split(cancer.data,cancer.target,random_state=0)
 #sns.heatmap(np.corrcoef(X_train.T), fmt='.2f', cmap='Blues')
+X_train = np.asarray(X_train)
+X_test = np.asarray(X_test)
+y_train = np.asarray(y_train)
+y_test = np.asarray(y_test)
 plt.show()
+
 
 etas = [0.1, 0.01, 0.001, 0.0001]
 accuracy = []
@@ -17,8 +22,11 @@ for eta in etas:
     #The NN has a single output node, the number of input nodes matches the number of features in the data
     #the NN becomes a logistic regression model
     network = ada.NN.Model(architecture=[[1, ada.activations.sigmoid]], eta=eta, epochs=100, optimizer='sgd', loss=ada.CE)
-    params = network.fit(X_train,y_train, X_test, y_test)[0]
-    accuracy.append(ada.util.accuracy(network.forward(network, X_test)[0], y_test))
+    loss,_=network.fit(X_train,y_train, X_test, y_test)
+    print(loss)
+    #print("--------" + str(network.classify(X_test).shape))
+    #print("--------" + str(y_test.shape))
+    accuracy.append(network.score(X_test,y_test))
 
 plt.plot(etas, accuracy)
 plt.xlabel('Learning rate')
